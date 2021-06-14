d51ee3fd091a45d3b8f3374bcfa297635c7cdb0b
Amaithi9876 <51565475+Amaithi9876@users.noreply.github.com>
2021-06-14 00:08:54 -0700

    If you run the below scritpt you will get “Code is Blasted” Explain Why the Code is blasted and how to diffuse it and get “Diffused”.
    var a = "2" > "12";
    //Don't touch below this
    if (a) {
      console.log("Code is Blasted")
    }
    else
    {
      console.log("Diffused") 
    }

---
+++ If you run the below scritpt you will get “Code is Blasted”.js
@@ -0,0 +1,19 @@
+var a = "2" > "12";
+//Don't touch below this
+if (a) {
+  console.log("Code is Blasted");
+}
+else
+{
+  console.log("Diffused");
+}
+// this bomb is blasted if u diffused it down the code is there
+var a = "2" < "12";
+//Don't touch below this
+if (a) {
+  console.log("Code is Blasted");
+}
+else
+{
+  console.log("Diffused");
+}
