349213458701dfa6259b7445c4e2fe462914d6c4
Amaithi9876 <51565475+Amaithi9876@users.noreply.github.com>
2021-06-14 00:02:29 -0700

    Fix the below to alert sum of two numbers
    let a = prompt("First number?");
    let b = prompt("Second number?");
    alert(a + b);

---
+++ Fix the below to alert sum of two numbers.js
@@ -0,0 +1,8 @@
+var a = prompt("First number");
+var b = prompt("Second number");
+alert(a + b);
+
+// but if u add a+b this is code 
+var a = prompt("First number?");
+var b = prompt("Second number?");
+alert(+a + +b);
